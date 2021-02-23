ccd87d0f3968c1fac73a0ede7e5638260ae69cb1
Elijah Blake <elijahblake12@gmail.com>
2021-02-23 00:47:30 -0700

    added comments

--- fizzbuzz/fortran/ElijahBlake/fizzbuzz.f90
+++ fizzbuzz/fortran/ElijahBlake/fizzbuzz.f90
@@ -1,12 +1,12 @@
 PROGRAM fizzbuzz
     IMPLICIT NONE
-    CHARACTER*100 :: w
-    INTEGER :: n
+    CHARACTER*100 :: nAsCharacter   !takes user input as a char
+    INTEGER :: n  
     INTEGER :: currentNumber
-    CALL get_command_argument(1, w)
-    READ(w,*) n
+    CALL get_command_argument(1, nAsCharacter)
+    READ(nAsCharacter,*) n !converts user input to integer
     
-    DO currentNumber=1,n
+    DO currentNumber=1,n    !loops up to n for FizzBuzz
         IF (MOD(currentNumber,15) == 0) THEN
             WRITE (*,*) "FizzBuzz"
         ELSE IF (MOD(currentNumber,5) == 0) THEN
