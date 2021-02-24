0dade24b06827d8678f7cc0165e0042d10401cce
Alyssa Newhart <67665773+alyssanewhart@users.noreply.github.com>
2021-02-23 16:12:58 -0800

    Update FizzBuzz.java

--- fizzbuzz/java/alyssanewhart/FizzBuzz.java
+++ fizzbuzz/java/alyssanewhart/FizzBuzz.java
@@ -3,15 +3,19 @@ public class FizzBuzz {
     public static void FizzBuzz(int n) {
 
         for (int i = 1; i <= n; i++) {
-            if (i % 15 == 0) System.out.println("FizzBuzz");
-            else if (i % 5 == 0) System.out.println("Buzz");
-            else if (i % 3 == 0) System.out.println("Fizz");
-            else System.out.println(i);
+           if (i % 15 == 0)
+                System.out.println("FizzBuzz");
+            else if (i % 5 == 0)
+                System.out.println("Buzz");
+            else if (i % 3 == 0)
+                System.out.println("Fizz");
+            else
+                System.out.println(i);
         }
     }
 
     public static void main(String[] args) {
-
+        
         int n = Integer.parseInt(args[0]);
         FizzBuzz(n);
     }
