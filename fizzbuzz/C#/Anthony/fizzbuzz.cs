0bd1a9b3dc919f328dd0211b0036d00ba1e54fdd
nardielloam <nardielloam@gmail.com>
2021-03-22 14:51:03 -0600

    adds Ping and Pong cases to the Fizzbuzz program

--- fizzbuzz/C#/Anthony/fizzbuzz.cs
+++ fizzbuzz/C#/Anthony/fizzbuzz.cs
@@ -16,6 +16,12 @@ namespace Anthony
                 if(i%5==0){
                     numberString = numberString+"Buzz";
                 }
+                if(i%7==0){
+                    numberString = numberString+"Ping";
+                }
+                if(i%11==0){
+                    numberString = numberString+"Pong";
+                }
                 if(numberString.Length==0){
                     numberString = numberString+i.ToString();
                 }
