05827983e2ac4cf37ec29917334a1a60ad3c3cca
Ransom Bowman <42879666+RansomBowman@users.noreply.github.com>
2021-03-23 18:18:14 -0600

    Working Testing

--- fizzbuzz/Python/RansomBowman/FizzBuzz.py
+++ fizzbuzz/Python/RansomBowman/FizzBuzz.py
@@ -3,26 +3,26 @@ import unittest
 
 def fizzBuzz(num):
     #num = int(input("Enter the number to FizzBuzz: "))
-
-    for i in range(1, num + 1):
-        if not(i%3 == 0) and not(i%5 == 0) and not (i%7 == 0) and not (i%11 == 0):
-            print(i)
-        else:
-            if (i%3 == 0):
-                print("Fizz", end = '')
-            if (i%5 == 0):
-                print("Buzz", end = '')
-            if (i%7 == 0):
-                print("Ping", end = '')
-            if (i%11 == 0):
-                print("Pong", end = '')
-            print()    
+    ret_str = ""
+    if not(num%3 == 0) and not(num%5 == 0) and not (num%7 == 0) and not (num%11 == 0):
+        return num
+    else:
+        if (num%3 == 0):
+            ret_str += "Fizz"
+        if (num%5 == 0):
+            ret_str += "Buzz"
+        if (num%7 == 0):
+            ret_str += "Ping"
+        if (num%11 == 0):
+            ret_str += "Pong"
+        return ret_str
 
 
 
 if __name__ == '__main__':
     num = int(str(sys.argv[1]))
+    
+    for i in range(1, num + 1):
+        print(fizzBuzz(i), end = '')
+        print()
 
-
-
-    fizzBuzz(num)
